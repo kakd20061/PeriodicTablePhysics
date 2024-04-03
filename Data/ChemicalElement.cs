d614df4e93a512603e7b1ee9c5de366cd972e9a5
Vitaminer <102184832+kakd20061@users.noreply.github.com>
2024-04-03 22:17:39 +0200

    isotopes

--- Data/ChemicalElement.cs
+++ Data/ChemicalElement.cs
@@ -12,6 +12,8 @@ public class Element
     public int group { get; set; }
     public int period { get; set; }
     public List<Usage> usage { get; set; }
+    
+    public List<Isotopes> isotopes { get; set; }
 }
 
 public class Usage
@@ -19,4 +21,12 @@ public class Usage
     public string name { get; set; }
     public string description { get; set; }
     public string photo { get; set; }
+}
+
+public class Isotopes
+{
+    public int mass { get; set; }
+    public float? naturalAbundance { get; set; }
+    public string symbol { get; set; }
+    public string name { get; set; }
 }
